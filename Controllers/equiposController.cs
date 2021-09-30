6b18440e4ec3dc527d7af7f1f49702a3dddf5768
ErickSiguache <erickaguilars403@gmail.com>
2021-09-30 16:33:02 -0600

    Consulta con union de tablas

--- Controllers/equiposController.cs
+++ Controllers/equiposController.cs
@@ -13,6 +13,10 @@ namespace _2017AS603.Controllers
             this._contexto = miContexto;
         }
 
+        /// <summary>
+        /// Metodo Select en general de la tabla equipos
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [Route("api/equipos")]
         public IActionResult Get(){
@@ -27,6 +31,43 @@ namespace _2017AS603.Controllers
             }
             ///De estar vacia muestra un error
             return NotFound();         
+        }
+
+        /// <summary>
+        /// Metodo Select en general de la tabla equipos con inner join
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        [Route("api/equiposInner")]
+        public IActionResult equiposInner()
+        {
+            ///"e" representa un alias para el listado de equipos y "tip" el listado de tipos de equipo
+            /// "mar" las marcas y "est" los estados
+            var equiposListInner = from e in _contexto.equipos
+                                   join tip in _contexto.tipo_equipo on e.tipo_equipo_id equals tip.id_tipo_equipo
+                                   join mar in _contexto.marcas on e.marca_id equals mar.id_marcas
+                                   join est in _contexto.estados_equipo on e.estado_equipo_id equals est.id_estados_equipo
+                                               select new {
+                                                   e.id_equipos,
+                                                   e.nombre,
+                                                   e.descripcion,
+                                                   tip_equipo_des = tip.descripcion, ///No pueden a ver dos propiedades
+                                                   mar.nombre_marca,                ///con el mismos nombre
+                                                   e.modelo,
+                                                   e.anio_compra,
+                                                   e.costo,
+                                                   e.vida_util,
+                                                   estado_equipo_des = est.estado, ///Por ello se coloca un alias 
+                                                   e.estado                        ///para no confundir al igual que en SQL Server
+                                               };
+
+            ///Verifica que la lista no este vacia y retorna los datos
+            if (equiposListInner.Count() > 0)
+            {
+                return Ok(equiposListInner);
+            }
+            ///De estar vacia muestra un error
+            return NotFound();
         }
 
         /// <summary>
@@ -145,7 +186,8 @@ namespace _2017AS603.Controllers
         /// <summary>
         /// Metodo de retorno de registros de equipos filtrado por el valor dado en el parametro
         /// </summary>
-        /// <param name="buscarNombre"> Representa el valor dado en el parametro en el campo Nombre </param>
+        /// <param name="buscarEstado"> Representa el valor dado en el parametro en el campo 
+        /// Descripcion de la tabla estado_marca </param>
         /// <returns></returns>
         [HttpGet]
         [Route("api/equipos/buscarequiestado/{buscarEstado}")]
